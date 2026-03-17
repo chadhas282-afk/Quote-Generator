86cb2d9340f1d7dd243d70ceabb29cbd2ea005d1
chadhas282-afk <chadhas282@gmail.com>
2026-03-17 18:09:29 +0530

    style: add randomQuote function to generate a random quote

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const quoteText = document.querySelector(".quote"),
 quoteBtn = document.querySelector("button"),
 authorName = document.querySelector(".name"),
 copyBtn = document.querySelector(".copy")
+
+function randomQuote(){
+    
+}
